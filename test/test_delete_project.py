5f0678f7931ce190091795bee7844a3cb76b53f5
pavelkalinchuk <715m4s@gmail.com>
2021-02-23 01:51:41 +0300

    Доработан тест по удалению проекта (добавлена предварительная проверка на наличие проектов и при их отсутствии добавляет новый проект)

--- test/test_delete_project.py
+++ test/test_delete_project.py
@@ -1,8 +1,13 @@
+from generator.project import project
+
+
 def test_delete_project(app, db):
-    old_project_list = db.get_project_list()
     app.session.login("administrator", "root")
-    project = old_project_list[0].name
-    app.project.delete_project(project)
+    if len(db.get_project_list()) == 0:
+        app.project.add_new_project(project)
+    old_project_list = db.get_project_list()
+    project_ = db.get_project_list()[0].name
+    app.project.delete_project(project_)
     new_project_list = db.get_project_list()
     assert len(old_project_list) - 1 == len(new_project_list)
-    assert project not in new_project_list
+    assert project_ not in new_project_list
