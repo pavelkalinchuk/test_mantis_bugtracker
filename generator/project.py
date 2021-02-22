b045c90a5d0381afea337aa900819f7afda50478
pavelkalinchuk <715m4s@gmail.com>
2021-02-23 01:02:47 +0300

    Доработан тест создания проекта (добавлены работающие assert'ы

--- generator/project.py
+++ generator/project.py
@@ -8,4 +8,5 @@ def generate_random_name():
     return "".join([random.choice(string.ascii_letters + string.digits) for i in range(random.randrange(5, 15))])
 
 
-name = (Project(name=generate_random_name()))
+project = (Project(name=generate_random_name()))
+# project_name = generate_random_name()
--- test/test_add_project.py
+++ test/test_add_project.py
@@ -1,10 +1,10 @@
-from generator.project import name
+from generator.project import project
 
 
 def test_add_new_group(app, db):
     old_project_list = db.get_project_list()
     app.session.login("administrator", "root")
-    app.project.add_new_project(name)
+    app.project.add_new_project(project)
     new_project_list = db.get_project_list()
     assert len(old_project_list) + 1 == len(new_project_list)
-    assert name == db.get_project_list()[-1]
+    assert project.name == new_project_list[-1].name
